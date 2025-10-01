262d374f9fae988a86d2d8a61dc5a3bfd92652f2
Andrew26012001 <andreychebotarevqw@gmail.com>
2025-10-01 14:48:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
 const CACHE = 'english-dict-v1';
-const urls = ['/', '/index.html', '/style.css', '/script.js', '/dictionary.js', '/lookup.js', '/utils.js'];
+const urls = ['/', '/index.html', '/style.css', '/script.js', '/dictionary.js', '/lookup.js', '/utils.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(urls)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 
 self.addEventListener('fetch', e => {
